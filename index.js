74454dacaba1d0bf2b6f356ea9e9e4afdea941ed
Peter Viss <peter.c.viss@biola.edu>
2019-06-22 19:31:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+const arr = [12, 13, 14, 15, 16, 20]
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+const random = Math.random() + 1
+const mod = 211 % 9
+const max = Math.max(12, 13, 14, 15, 16, 20)
